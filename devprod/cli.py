dc89b86688f13e4b766bfef9a5d26dc21af095dc
Damien Nozay <damiennozay+github@gmail.com>
2020-02-27 20:37:12 -0800

    basic cli for github search

--- devprod/cli.py
+++ devprod/cli.py
@@ -1,6 +1,33 @@
+import click
+
 from .version import __version__
 
 
+@click.group()
 def main():
-    print('ok')
+    pass
+
+
+@main.group()
+def github():
+    pass
+
+
+@github.command('search')
+@click.argument('query')
+def github_search(query):
+    from devprod import env
+    from devprod.github import session, search
+    env.load_environment(['GITHUB_TOKEN'])
+    s = session(env.GITHUB_TOKEN)
+    results = search(s, query)
+    for path in sorted(results.keys()):
+        print(f'{path}:')
+        for url in results[path]:
+            print(f'\t- {url}')
+        print('')
+
+
+@main.command()
+def version():
     print(f'version: {__version__}')
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,32 @@
-import devprod.cli
+from click.testing import CliRunner
+from unittest.mock import patch
+
+from devprod import cli
 
 
 def test_cli_main():
-    devprod.cli.main()
+    result = CliRunner().invoke(cli.main, [])
+    assert result.exit_code == 0
+
+
+def test_cli_version():
+    result = CliRunner().invoke(cli.main, ['version'])
+    assert result.exit_code == 0
+
+
+def test_cli_github():
+    result = CliRunner().invoke(cli.main, ['github'])
+    assert result.exit_code == 0
+
+
+@patch('devprod.github.search')
+def test_cli_github_search(search):
+    result = CliRunner().invoke(cli.main, ['github', 'search'])
+    assert result.exit_code == 2
+
+    search.return_value = {'filepath': ['url1', 'url2']}
+    result = CliRunner().invoke(cli.main, ['github', 'search', 'some-query'])
+    assert result.exit_code == 0
+    assert 'filepath' in result.stdout
+    assert 'url1' in result.stdout
+    assert 'url2' in result.stdout
